dc0e512b23bf4d590a6e9d53664f60264767b7d6
hzy <3365875546@qq.com>
2023-04-28 19:09:20 +0800

    2023-04-28

--- JQuery/练习/html/电梯导航/js/index.js
+++ JQuery/练习/html/电梯导航/js/index.js
@@ -1,15 +1,30 @@
 $(function () {
     // 1. 显示隐藏电梯导航
+    var flag = true;
     var boxTop = $('.recommend').offset().top
-    $(window).scroll(function () {
+    toggleTool();
+    function toggleTool() {
         if ($(document).scrollTop() >= boxTop) {
             $('.fixedtool').fadeIn()
         } else {
             $('.fixedtool').fadeOut()
         }
+    }
+    $(window).scroll(function () {
+        toggleTool();
+        // 页面滚动到某个内容区域，左侧电梯导航li相应添加和删除current类名
+        if (flag) {
+            $('.floor .w').each(function (i, ele) {
+                if ($(document).scrollTop() >= $(ele).offset().top) {
+                    // console.log(i);
+                    $('.fixedtool li').eq(i).addClass('current').siblings().removeClass()
+                }
+            })
+        }
     })
     // 2.点击电梯导航页面可以滚动到相应内容区域
     $(".fixedtool li").click(function () {
+        flag = false
         // console.log($(this).index());
         // 当每次点击li 就需要计算出页面要去往的位置
         // 选出对应索引号的内容区的盒子 计算它的.offset().top
@@ -17,6 +32,10 @@ $(function () {
         // 页面滚动效果
         $('body,html').stop().animate({
             scrollTop: curent,
+        }, function () {
+            flag = true
         })
+        // 点击之后，让当前的li，添加current类名，其余的移除current类名
+        $(this).addClass('current').siblings().removeClass();
     })
 })
