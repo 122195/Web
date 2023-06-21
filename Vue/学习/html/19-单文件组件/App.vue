1f3df6195a0b6cf9174ddbf2edca649102fab6b8
hzy <3365875546@qq.com>
2023-06-21 15:09:37 +0800

    2023-06-21

---
+++ Vue/学习/html/19-单文件组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+// 引入组件
+import School from './school.vue';
+import Student from './student.vue';
+export default {
+    name:'App',
+    components:{
+        School,
+        Student
+    }
+}
+</script>
+
+<style>
+
+</style>
