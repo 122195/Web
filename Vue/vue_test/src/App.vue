153ab5c367d215803daa0f99804c2834f861a110
hzy <3365875546@qq.com>
2023-06-22 22:12:56 +0800

    2023-06-22

--- Vue/vue_test/src/App.vue
+++ Vue/vue_test/src/App.vue
@@ -1,23 +1,14 @@
 <template>
-    <div>
-        <School></School>
-        <Student></Student>
-    </div>
+	<div>
+		<Student name="李四" sex="女" :age="18"/>
+	</div>
 </template>
 
 <script>
-// 引入组件
-import School from './components/school.vue';
-import Student from './components/student.vue';
-export default {
-    name:'App',
-    components:{
-        School,
-        Student
-    }
-}
+	import Student from './components/Student'
+
+	export default {
+		name:'App',
+		components:{Student},
+	}
 </script>
-
-<style>
-
-</style>
--- Vue/vue_test/src/components/student.vue
+++ Vue/vue_test/src/components/student.vue
@@ -1,20 +1,53 @@
 <template>
-    <div class="demo">
-        <!-- 组件的结构 -->
-        <h2>学生名称：{{name}}</h2>
-        <h2>学生地址：{{age}}</h2>
-    </div>
+	<div>
+		<h1>{{msg}}</h1>
+		<h2>学生姓名：{{name}}</h2>
+		<h2>学生性别：{{sex}}</h2>
+		<h2>学校年龄：{{MyAge}}</h2>
+		<button @click="updateAge">尝试修改年龄</button>
+	</div>
 </template>
 
 <script>
-    // 组件交互相关的代码(数据、方法等等)
-    export default {
-        name:'MyStudent',
-        data() {
-            return {
-                name:'张三',
-                age:18
-            }
-        },
-    }
+	export default {
+		name:'MyStudent',
+		data() {
+			return {
+				msg:'我是一个爱学习的好学生',
+				MyAge:this.age
+				// name:'李白',
+				// sex:'男',
+				// age:18,
+			}
+		},
+		methods: {
+			updateAge(){
+				this.MyAge++
+			}
+		},
+		// 简单声明接收
+		props:['name','age','sex']
+		// 接收的同时对数据进行类型限制
+		/* props:{
+			name:String,
+			age:Number,
+			sex:String
+		} */
+
+		// 接收的同时对数据：进行类型限制+默认值的指定+必要性的限制
+		/* props:{
+			name:{
+				type:String, // name的类型是字符串
+				required:true // name是必要的
+			},
+			age:{
+				type:Number,
+				default:99 // 默认值
+			},
+			sex:{
+				type:String,
+				required:true
+			}
+		} */
+	}
 </script>
